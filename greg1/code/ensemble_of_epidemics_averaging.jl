ffa3e0627d6fbb238d6e108a8d4e5f520383deb8
gbritten <gregleebritten@gmail.com>
2020-06-25 09:53:53 -0400

    ensemble averaging

---
+++ greg1/code/ensemble_of_epidemics_averaging.jl
@@ -0,0 +1,58 @@
+include("stochastic_tools.jl")
+using Statistics
+using Interpolations
+
+n_people = 1000
+β₀ = 0.5
+
+problem = stochastic_SIR_problem(n_people; β = β₀/n_people, γ = 0.1)
+
+ensemble = solve_ensemble(problem, 100)
+
+p = plot_solution(ensemble[1], alpha=0.1)
+
+for i = 2:length(ensemble)
+    plot_solution!(p, ensemble[i], alpha=0.1)
+end
+
+display(p)
+
+##--Extract and interpolate--#########
+t, S, I, R = unpack(ensemble[2])
+
+t_intp = 1.0:0.1:100
+Sint   = LinearInterpolation(t,S)
+Iint   = LinearInterpolation(t,I)
+Rint   = LinearInterpolation(t,R)
+sint   = Sint(t_intp)
+iint   = Iint(t_intp)
+rint   = Rint(t_intp)
+
+plot(t_intp,[sint iint rint])
+
+
+###################################
+
+E[:,:,1] = [sint iint rint]
+plot(t_intp,E[:,:,1])
+
+##--LOOP--############
+E = Array{Float64}(undef,length(t_intp),3,100)
+t_intp = 1.0:0.1:100
+
+for i = 1:100
+    t, S, I, R = unpack(ensemble[i])
+
+    Sint   = LinearInterpolation(t,S)
+    Iint   = LinearInterpolation(t,I)
+    Rint   = LinearInterpolation(t,R)
+    sint   = Sint(t_intp)
+    iint   = Iint(t_intp)
+    rint   = Rint(t_intp)
+
+    E[:,:,i] = [sint iint rint]
+end
+
+E_mean = dropdims(mean(E,dims=3),dims=3)
+
+plot(t_intp,E_mean)
