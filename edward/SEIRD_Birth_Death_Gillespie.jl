238466833458faf327f2f993cab1fce192a13b71
Edward Gathuru <eggathuru@pm.me>
2020-07-28 21:27:41 -0500

    Added tau leaping and fixed ssa bug

--- edward/SEIRD_Birth_Death_Gillespie.jl
+++ edward/SEIRD_Birth_Death_Gillespie.jl
@@ -1,4 +1,4 @@
-using Random, Plots
+using Random, Plots, Distributions
 
 struct My_Parameters
     μ; β; a; γ; α
@@ -7,49 +7,48 @@ end
 const N_SIM = Int(1e4)
 const N_COMPARTMENT = 5
 
-const u = zeros(Int, N_COMPARTMENT, N_SIM+1)
+const u = zeros(Float64, N_COMPARTMENT, N_SIM+1)
 const T = zeros(N_SIM+1)
 
-function birth!(u, i)
-    u[1, i] += 1
+function birth!(u, i, v = 1)
+    u[1, i] += v
 end
 
-function susceptible_to_exposed!(u, i)
-    u[1, i] -= 1
-    u[2, i] += 1
+function susceptible_to_exposed!(u, i, v = 1)
+    u[1, i] -= v
+    u[2, i] += v
 end
 
-function exposed_to_infectious!(u, i)
-    u[2, i] -= 1
-    u[3, i] += 1
+function exposed_to_infectious!(u, i, v = 1)
+    u[2, i] -= v
+    u[3, i] += v
 end
 
-function infectious_to_resistant!(u, i)
-    u[3, i] -= 1
-    u[4, i] += 1
+function infectious_to_resistant!(u, i, v = 1)
+    u[3, i] -= v
+    u[4, i] += v
 end
 
 # Death events
-function susceptible_death!(u, i)
-    u[1, i] -= 1
-    return nothing
+function susceptible_death!(u, i, v = 1)
+    u[1, i] -= v
 end
 
-function exposed_death!(u, i)
-    u[2, i] -= 1
+function exposed_death!(u, i, v = 1)
+    u[2, i] -= v
 end
 
-function infected_death!(u, i)
-    u[3, i] -= 1
-    u[5, i] += 1
+function infected_death!(u, i, v = 1)
+    u[3, i] -= v
+    u[5, i] += v
 end
 
-function recovered_death!(u, i)
-    u[4, i] -= 1
+function recovered_death!(u, i, v = 1)
+    u[4, i] -= v
 end
 
 # Rates
-birth_rate(state, parameters) = parameters.μ * sum(state)
+birth_rate(state, parameters) = parameters.μ * sum(state[1:4])
 
 exposure_rate(state, parameters) = parameters.β * state[1] * state[3] / (sum(state[1:4]))
 infection_rate(state, parameters) = parameters.a * state[2]
@@ -60,22 +59,24 @@ exposed_death_rate(state, parameters) = parameters.μ * state[2]
 infected_death_rate(state, parameters) = (parameters.α+parameters.μ) * state[3]
 recovered_death_rate(state, parameters) = parameters.μ * state[4]
 
+function a_i(state, parameters)
+    return [
+        birth_rate(state, parameters),
+
+        exposure_rate(state, parameters),
+        infection_rate(state, parameters),
+        recovery_rate(state, parameters),
+
+        susceptible_death_rate(state, parameters),
+        exposed_death_rate(state, parameters),
+        infected_death_rate(state, parameters),
+        recovered_death_rate(state, parameters),
+    ]
+end
 
 function ∑a(state, parameters, i)
-    a_i =  [birth_rate(state, parameters),
-
-            exposure_rate(state, parameters),
-            infection_rate(state, parameters),
-            recovery_rate(state, parameters),
-
-            susceptible_death_rate(state, parameters),
-            exposed_death_rate(state, parameters),
-            infected_death_rate(state, parameters),
-            recovered_death_rate(state, parameters),
-            ]
-    # sort!(a_i)
-    # @show a_i
-    return sum(a_i[1:i])
+    a_t = a_i(state, parameters)
+    return sum(a_t[1:i])
 end
 
 function direct_ssa(initial_condition, parameters)
@@ -113,7 +114,33 @@ function direct_ssa(initial_condition, parameters)
             recovered_death!(u, i+1)
         end
 
-        T[i+1] = τ
+        T[i+1] = τ + T[i]
+    end
+end
+
+function tau_leap(initial_condition, parameters, τ)
+    u[:, 1] .= initial_condition
+    T[1] = 1
+
+    for i in 1:N_SIM
+        state = u[:, i]
+        u[:, i+1] .= state
+
+        a_t = a_i(state, parameters)
+        p = [Poisson(τ*a_t[i]) for i in 1:8]
+
+        birth!(u, i+1, rand(p[1]))
+
+        susceptible_to_exposed!(u, i+1, rand(p[2]))
+        exposed_to_infectious!(u, i+1, rand(p[3]))
+        infectious_to_resistant!(u, i+1, rand(p[4]))
+
+        susceptible_death!(u, i+1, rand(p[5]))
+        exposed_death!(u, i+1, rand(p[6]))
+        infected_death!(u, i+1, rand(p[7]))
+        recovered_death!(u, i+1, rand(p[8]))
+
+        T[i+1] = τ + T[i]
     end
 end
 
@@ -123,7 +150,7 @@ function unpack(u)
     I = u[3, :]
     R = u[4, :]
     D = u[5, :]
-    t = cumsum(T)
+    t = T
     return t, S, E, I, R, D
 end
 
@@ -137,8 +164,12 @@ function plot_solution(u)
                 linetype=:steppost)
 end
 
-μ=0.01; β=0.07; a=0.07; γ=0.006; α=0.02;
+μ=0.01; β=0.055; a=0.07; γ=0.006; α=0.005;
 parameters = My_Parameters(μ, β, a, γ, α)
-initial_condition = 950, 0, 50, 0, 0
+initial_condition = 990, 0, 10, 0, 0
+
 direct_ssa(initial_condition, parameters)
+plot_solution(u)
+
+tau_leap(initial_condition, parameters, 0.03)
 plot_solution(u)
